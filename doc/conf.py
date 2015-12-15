1d91c53fe6fef449ce142444fbf9562d46403ff0
Matthias Geier <Matthias.Geier@gmail.com>
2015-12-15 18:46:09 +0100

    DOC: Switch to cloud theme

--- doc/conf.py
+++ doc/conf.py
@@ -4,6 +4,8 @@
 # Use sphinx-quickstart to create your own conf.py file!
 # After that, you have to edit a few things.  See below.
 
+import cloud_sptheme
+
 # Select nbsphinx and, if needed, add a math extension (mathjax or pngmath):
 extensions = [
     'nbsphinx',
@@ -103,6 +105,8 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'cloud'  # or 'redcloud'
+html_theme_path = [cloud_sptheme.get_theme_dir()]
 
 # -- Options for LaTeX output ---------------------------------------------
 
