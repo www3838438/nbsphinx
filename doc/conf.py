7c70b9ea50957ab6e6add07dd916e40d6a9bb39f
Matthias Geier <Matthias.Geier@gmail.com>
2016-02-27 13:33:33 +0100

    DOC: Switch to pyramid theme

--- doc/conf.py
+++ doc/conf.py
@@ -103,6 +103,7 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'pyramid'
 
 # -- Options for LaTeX output ---------------------------------------------
 
