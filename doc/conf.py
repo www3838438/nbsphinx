ad5eaeeb2befc3b52a98c4399bf6befc629e1b61
Matthias Geier <Matthias.Geier@gmail.com>
2016-02-27 20:31:48 +0100

    DOC: Switch to dotted theme

--- doc/conf.py
+++ doc/conf.py
@@ -103,6 +103,7 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'dotted'
 
 # -- Options for LaTeX output ---------------------------------------------
 
