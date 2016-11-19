62be69fb2187df0df7d10615bb1c07e8f8c1df61
Matthias Geier <Matthias.Geier@gmail.com>
2016-11-19 10:08:21 +0100

    Add option nbsphinx_save_rst

--- src/nbsphinx.py
+++ src/nbsphinx.py
@@ -663,6 +663,17 @@ class NotebookParser(rst.Parser):
             with open(dest, 'wb') as f:
                 f.write(data)
 
+        if env.config.nbsphinx_save_rst:
+            auxfile = env.doc2path(env.docname, base=auxdir, suffix='.rst')
+            rstfile = env.doc2path(env.docname, base=None, suffix='.rst')
+            sphinx.util.ensuredir(
+                os.path.join(auxdir, os.path.dirname(rstfile)))
+            with open(os.path.join(env.srcdir, auxfile), 'w') as f:
+                f.write(rststring)
+            if not hasattr(env, 'nbsphinx_rstfiles'):
+                env.nbsphinx_rstfiles = {}
+            env.nbsphinx_rstfiles[env.docname] = auxfile, rstfile
+
         if resources.get('nbsphinx_orphan', False):
             env.metadata[env.docname]['orphan'] = ''
 
@@ -1238,6 +1249,12 @@ def html_collect_pages(app):
             sphinx.util.copyfile(os.path.join(app.env.srcdir, file), target)
         except OSError as err:
             app.warn('cannot copy local file {!r}: {}'.format(file, err))
+
+    rstfiles = getattr(app.env, 'nbsphinx_rstfiles', {}).values()
+    for source, target in status_iterator(
+            rstfiles, 'copying RST files ... ',
+            sphinx.util.console.brown, len(rstfiles)):
+        sphinx.util.copyfile(source, os.path.join(app.builder.outdir, target))
     return []  # No new HTML pages are created
 
 
@@ -1245,6 +1262,10 @@ def env_purge_doc(app, env, docname):
     """Remove list of local files for a given document."""
     try:
         del env.nbsphinx_files[docname]
+    except (AttributeError, KeyError):
+        pass
+    try:
+        del env.nbsphinx_rstfiles[docname]
     except (AttributeError, KeyError):
         pass
 
@@ -1378,6 +1399,7 @@ def setup(app):
     app.add_config_value('nbsphinx_responsive_width', '540px', rebuild='html')
     app.add_config_value('nbsphinx_prolog', None, rebuild='env')
     app.add_config_value('nbsphinx_epilog', None, rebuild='env')
+    app.add_config_value('nbsphinx_save_rst', False, rebuild='env')
 
     app.add_directive('nbinput', NbInput)
     app.add_directive('nboutput', NbOutput)
