16453f21b9c2c3fe10ea64c43cfae392c025c198
Matthias Geier <Matthias.Geier@gmail.com>
2017-03-12 11:10:54 +0100

    DOC: Switch to jupyter theme

--- doc/conf.py
+++ doc/conf.py
@@ -4,6 +4,8 @@
 # Use sphinx-quickstart to create your own conf.py file!
 # After that, you have to edit a few things.  See below.
 
+import jupyter_sphinx_theme
+
 # Select nbsphinx and, if needed, add a math extension (mathjax or pngmath):
 extensions = [
     'nbsphinx',
@@ -103,6 +105,13 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'jupyter'
+html_theme_path = jupyter_sphinx_theme.get_html_theme_path()
+html_sidebars = {
+    '**': [
+        'sidebartoc.html'
+    ]
+}
 
 # -- Options for LaTeX output ---------------------------------------------
 
